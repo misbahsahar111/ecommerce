ae8a4154345d0ea43d41d3acbde2d97b6ec39bd5
Misbah Sahar <misbahsahar08@gmail.com>
2022-06-28 14:28:21 -0400

    added pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products=Product.all
+    @products=Product.paginate(page: params[:page], per_page: 2)
     @cart_item = current_order.cart_items.new
   end
 
